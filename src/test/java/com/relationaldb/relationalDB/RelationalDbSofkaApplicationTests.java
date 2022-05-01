94d24ddfe1c6a33bfea05699c1e91a8e003a8a02
jpabloallin <jpac.647@gmail.com>
2022-05-01 15:54:31 -0500

    Project creation

---
+++ src/test/java/com/relationaldb/relationalDB/RelationalDbSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relationaldb.relationalDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDbSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
